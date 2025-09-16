116c5159579ab1200a3bb5ae7d5577c4af20a455
Abhishek Thakur <144336328+abhithakjutre@users.noreply.github.com>
2025-09-16 15:46:19 +0530

    This Updated Website

---
+++ MODULE 15/1 HELLO WORD USING REST API/helloword/helloapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import APIView
+# Create your views here.
+
+class Helloword_view(APIView):
+    def get(self,request):
+        return Response({"message": "Hello, world!"})
