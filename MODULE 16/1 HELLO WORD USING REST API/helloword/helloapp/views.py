504333293c6a355fcd8b57675437be539b372a84
Abhishek Thakur <144336328+abhithakjutre@users.noreply.github.com>
2025-09-19 09:57:48 +0530

    This is updated code

--- MODULE 16/1 HELLO WORD USING REST API/helloword/helloapp/views.py
+++ MODULE 16/1 HELLO WORD USING REST API/helloword/helloapp/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from helloapp.serializers import PostSerializer
 from rest_framework.viewsets import ModelViewSet
 from helloapp.models import Post
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class Helloword_view(APIView):
@@ -11,5 +12,6 @@ class Helloword_view(APIView):
         return Response({"message": "Hello, world!"})
 
 class PostView(ModelViewSet):
-    queryset = Post.objects.all()
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all().order_by('-created_on')
     serializer_class = PostSerializer
